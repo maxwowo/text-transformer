68b89996d8ff125be29ed2f4a63a9e6baa862d15
Max Wo <maxwo@protonmail.com>
2019-05-08 22:05:50 +1000

    Added basic boxes

--- src/components/Cockpit/DisplayBox/DisplayBox.js
+++ src/components/Cockpit/DisplayBox/DisplayBox.js
@@ -1,28 +1,27 @@
 // React
 import React from "react";
-
 // Material UI components
-import TextField from "@material-ui/core/TextField";
-
+import Paper from '@material-ui/core/Paper';
+import Typography from "@material-ui/core/Typography";
 // Style tool
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
 
 const styles = theme => ({
-  textField: {
-    width: "80%"
+  displayBox: {
+    width: "70%",
+    margin: "auto",
+    marginTop: "20px",
+    minHeight: "25vh",
+    padding: 40
   }
 });
 
 const DisplayBox = props => (
-  <TextField
-    id="outlined-multiline-static"
-    label="Multiline"
-    multiline
-    rows="15"
-    className={props.classes.textField}
-    margin="0"
-    variant="outlined"
-  />
+  <Paper className={props.classes.displayBox} elevation={4}>
+    <Typography variant="h2" color="inherit" className={props.classes.displayText}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum deleniti dolore illo
+    </Typography>
+  </Paper>
 );
 
 export default withStyles(styles)(DisplayBox);
--- src/components/Cockpit/InputBox/InputBox.js
+++ src/components/Cockpit/InputBox/InputBox.js
@@ -1,30 +1,36 @@
 // React
 import React from "react";
-
 // Material UI components
 import TextField from "@material-ui/core/TextField";
-
+import Paper from '@material-ui/core/Paper';
 // Style tool
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
 
 const styles = theme => ({
+  textPaper: {
+    marginTop: "40px",
+    width: "70%",
+    margin: "auto",
+    padding: 40
+  },
   textField: {
-    width: "80%"
+    width: "100%"
   }
 });
 
 const InputBox = props => (
-  <TextField
-    id="outlined-multiline-static"
-    label="Multiline"
-    multiline
-    rows="15"
-    className={props.classes.textField}
-    variant="outlined"
-    inputProps={{
-      style: { textAlign: "center" }
-    }}
-  />
+  <Paper className={props.classes.textPaper} elevation={4}>
+    <TextField
+      id="outlined-multiline-static"
+      multiline
+      rows="10"
+      className={props.classes.textField}
+      inputProps={{
+        style: {textAlign: "center", fontSize: "3rem"}
+      }}
+      autoFocus={true}
+    />
+  </Paper>
 );
 
 export default withStyles(styles)(InputBox);
