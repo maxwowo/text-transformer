9bf85f25b5f4d4823556d7ed0f7922d7482a3e65
Max Wo <maxwo@protonmail.com>
2019-05-09 11:58:04 +1000

    Added option bar to input text field

--- src/components/Cockpit/DisplayBox/DisplayBox.js
+++ src/components/Cockpit/DisplayBox/DisplayBox.js
@@ -1,18 +1,23 @@
 // React
 import React from "react";
+
 // Material UI components
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+
 // Style tool
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
+
+// Constants
+import { DISPLAY_PAPER_PADDING, PAPER_MARGIN_TOP, PAPER_WIDTH } from "../../../constants";
 
 const styles = theme => ({
   displayBox: {
-    width: "70%",
+    width: `calc(${PAPER_WIDTH})`,
     margin: "auto",
-    marginTop: "20px",
+    marginTop: PAPER_MARGIN_TOP,
     minHeight: "25vh",
-    padding: 40
+    padding: DISPLAY_PAPER_PADDING
   }
 });
 
--- src/components/Cockpit/InputBox/InputBox.js
+++ src/components/Cockpit/InputBox/InputBox.js
@@ -1,17 +1,24 @@
 // React
 import React from "react";
+
 // Material UI components
 import TextField from "@material-ui/core/TextField";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
+
 // Style tool
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
+
+// Input box components
+import ProcessorOptions from "./ProcessorOptions/ProcessorOptions";
+
+// Constants
+import { DISPLAY_PAPER_PADDING, INPUT_TEXTFIELD_PADDING_TOP, PAPER_MARGIN_TOP, PAPER_WIDTH } from "../../../constants";
 
 const styles = theme => ({
   textPaper: {
-    marginTop: "40px",
-    width: "70%",
-    margin: "auto",
-    padding: 40
+    marginTop: PAPER_MARGIN_TOP,
+    width: `calc(${PAPER_WIDTH} + ${2 * DISPLAY_PAPER_PADDING}px)`,
+    margin: "auto"
   },
   textField: {
     width: "100%"
@@ -20,13 +27,14 @@ const styles = theme => ({
 
 const InputBox = props => (
   <Paper className={props.classes.textPaper} elevation={4}>
+    <ProcessorOptions/>
     <TextField
       id="outlined-multiline-static"
       multiline
       rows="10"
       className={props.classes.textField}
       inputProps={{
-        style: {textAlign: "center", fontSize: "3rem"}
+        style: {textAlign: "center", fontSize: "3rem", paddingTop: INPUT_TEXTFIELD_PADDING_TOP}
       }}
       autoFocus={true}
     />
